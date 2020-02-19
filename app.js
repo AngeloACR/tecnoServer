c1fcf2bcdbfc04e43fd89fe11b9627eb2b615bd4
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-02-19 03:00:07 -0400

    Building my site

---
+++ app.js
@@ -0,0 +1,40 @@
+const localServer = require('./localServer');
+const path = require('path');
+/* 
+const webListenerPort = 4300;
+const webFolder = './public/samaFront';
+const webPath = webFolder + '/index.html';
+const webApp = webListener.init(webFolder, webListenerPort);
+
+// webApp.get('/', (req, res) => {
+// 	res.send('We are having some troubles, please come back in a while!');
+// });
+
+// 	//Pointing to angular app
+// webApp.get('/*', (req,res) => {
+// 	var fileToSend = path.join(__dirname, webPath);
+// 	res.sendFile(fileToSend);
+// });
+
+webApp.listen(webListenerPort, () => {
+    console.log('Web Listener running at: ' + webListenerPort);
+}); */
+
+const localPort = 3400;
+const localFolder = '../public/construction';
+const localPath = localFolder + '/index.html';
+const localApp = localServer.init(localFolder, localPort);
+
+localApp.get('/', (req, res) => {
+    res.send('We are having some troubles, please come back in a while!');
+});
+
+//Pointing to angular app
+localApp.get('/*', (req, res) => {
+    var fileToSend = path.join(__dirname, localPath);
+    res.sendFile(fileToSend);
+});
+
+localApp.listen(localPort, () => {
+    console.log('Server running at: ' + localPort);
+});
---
+++ config/env.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+// DATABASES ADDRESSES
+	prodDB: "mongodb://angeloacr:MiUser47@ds129600.mlab.com:29600/hustlerweb",
+	testDB: "mongodb://localhost:27017/hustleWeb",
+	
+// SECRETS FOR ENCRYPTION
+	authSecret: 'Shall I let you pass, or Shall I not?',
+	vSecret: 'Becoming an outstanding Meat Box',
+	cSecret: 'My incredible MeatBox Session',
+
+// SOME OTHER IMPORTANT STUFF
+	contactMail: 'angeloacr91@gmail.com',
+	contactPass: 'MiGmail47'
+}
